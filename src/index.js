6946569f94c315cdd6f70a55af62f450ae3935b9
rubis-design <rubis.smm@gmail.com>
2024-08-04 21:48:39 +0300

    add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let nStr = Math.abs(n).toString();
+    let reversedStr = '';
+
+    for (let i = nStr.length - 1; i >= 0; i--) {
+        reversedStr += nStr[i];
+    }
+    return parseInt(reversedStr, 10);
 }
