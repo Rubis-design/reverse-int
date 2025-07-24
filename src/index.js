0476acdea8d670be0de81638d487125ad292e0d4
Rubis-design <151998802+Rubis-design@users.noreply.github.com>
2025-07-24 12:35:02 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
-};
+function reverse(n) {
+  const digits = Math.abs(n).toString().split('').reverse().join('');
+  return Number(digits);
+}
+
+module.exports = reverse;
